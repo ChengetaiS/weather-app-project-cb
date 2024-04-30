f8be9d6583a1d17d8330348595fafff0b5e643c7
ChengetaiS <162143946+ChengetaiS@users.noreply.github.com>
2024-05-01 01:59:28 +0200

    Added function for date display  for weather forecast iconutilizing forecast api and added css

--- src/index.js
+++ src/index.js
@@ -61,6 +61,12 @@ function searchingForCity(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "607bfb3coe77c132397e1ee35e4f2cat";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -69,23 +75,26 @@ function getForecast(city) {
 
 function displayForecast(response) {
   let forecast = document.querySelector("#weather-forecast");
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-              <div class="weather-forecast-date">${day}</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+              <div class="weather-forecast-date">${formatDay(day.time)}</div>
               <div>
                 <img
-                  src="https://openweathermap.org/img/wn/10d@2x.png"
-                  alt=""
-                  width="42"
+                  src="${day.condition.icon_url}" class="weather-forecast-icon"
                 />
               </div>
               <div class="weather-forecast-temperature">
-                <span class="weather-forecast-temp-max"><strong>20&deg</strong></span>
-                <span class="weather-forecast-temp-min">15&deg</span></div></div>`;
+                <span class="weather-forecast-temp-max"><strong>${Math.round(
+                  day.temperature.maximum
+                )}&deg</strong></span>
+                <span class="weather-forecast-temp-min">${Math.round(
+                  day.temperature.minimum
+                )}&deg</span></div></div>`;
+    }
   });
   forecast.innerHTML = forecastHtml;
 }
